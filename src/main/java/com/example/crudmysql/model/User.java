bd7aa958425f4b0fab3795d11e0ea1b353862da2
Merwa95 <merwamanssar@gmail.com>
2020-11-13 17:09:08 +0100

    add dependency Lombok

--- src/main/java/com/example/crudmysql/model/User.java
+++ src/main/java/com/example/crudmysql/model/User.java
@@ -1,9 +1,14 @@
 package com.example.crudmysql.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.*;
 
 @Entity
 @Table (name = "users")
+@Getter
+@Setter
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
@@ -14,36 +19,4 @@ public class User {
     private String lastName;
     @Column(name="email")
     private String email;
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
 }
