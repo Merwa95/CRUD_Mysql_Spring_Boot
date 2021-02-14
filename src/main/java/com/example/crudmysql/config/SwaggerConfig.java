af36cc27eb1ac5661a593231170773f47a4a6961
Merwa95 <merwamanssar@gmail.com>
2021-02-14 23:56:37 +0100

    update_OAUTH2_Swagger

--- src/main/java/com/example/crudmysql/config/SwaggerConfig.java
+++ src/main/java/com/example/crudmysql/config/SwaggerConfig.java
@@ -2,18 +2,27 @@ package com.example.crudmysql.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import springfox.bean.validators.configuration.BeanValidatorPluginsConfiguration;
+import springfox.documentation.builders.AuthorizationCodeGrantBuilder;
+import springfox.documentation.builders.OAuthBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
+import springfox.documentation.service.*;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger.web.SecurityConfiguration;
+import springfox.documentation.swagger.web.SecurityConfigurationBuilder;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+
+import java.util.Arrays;
 import java.util.Collections;
 
 @Configuration
 @EnableSwagger2
+//@Import(BeanValidatorPluginsConfiguration.class)
 public class SwaggerConfig {
 
     @Bean
@@ -24,7 +33,58 @@ public class SwaggerConfig {
                 .apis(RequestHandlerSelectors.basePackage("com.example.crudmysql.api"))
                 .paths(PathSelectors.ant("/api/*"))
                 .build()
-                .apiInfo(apiInfo());  }
+                .apiInfo(apiInfo())
+                .securitySchemes (Arrays.asList (securityScheme ()))
+                .securityContexts (Arrays.asList (securityContext ()));  }
+
+
+
+    // definir quelque valeurs par defaut
+    @Bean
+    public SecurityConfiguration security () {
+        return SecurityConfigurationBuilder.builder ()
+                .clientId ("Merwa")
+                .clientSecret ("1234")
+                .scopeSeparator ("")
+                .useBasicAuthenticationWithAccessCodeGrant (true)
+                .build ();
+    }
+
+    // decrire comment schema OAUTH2
+
+    private SecurityScheme securityScheme() {
+        GrantType grantType = new AuthorizationCodeGrantBuilder()
+                .tokenEndpoint(new TokenEndpoint("" + "/token", "oauthtoken"))
+                .tokenRequestEndpoint(
+                        new TokenRequestEndpoint("" + "/authorize", "Merwa", "1234"))
+                .build();
+
+        SecurityScheme oauth = new OAuthBuilder().name("spring_oauth")
+                .grantTypes(Arrays.asList(grantType))
+                .scopes(Arrays.asList(scopes()))
+                .build();
+        return oauth;
+    }
+
+
+    //les portées que nous devons avoir définies
+    private AuthorizationScope[] scopes() {
+        AuthorizationScope[] scopes = {
+                new AuthorizationScope("read", "for read operations"),
+                new AuthorizationScope("write", "for write operations"),
+                new AuthorizationScope("api", "Access api API") };
+        return scopes;
+    }
+
+
+    // définir un contexte de sécurité de l'API
+    private SecurityContext securityContext () {
+        return SecurityContext.builder ()
+                .securityReferences (
+                        Arrays.asList (new SecurityReference ("spring_oauth", scopes ())))
+                .forPaths (PathSelectors.regex ("/ api. *"))
+                .build ( );
+    }
 
     private ApiInfo apiInfo() {
         return new ApiInfo(
