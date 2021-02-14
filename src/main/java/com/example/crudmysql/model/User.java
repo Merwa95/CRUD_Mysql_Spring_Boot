4d59c628c21c8cffde3a471b2a17d34e2165e077
Merwa95 <merwamanssar@gmail.com>
2021-02-14 23:56:37 +0100

    Add_OAUTH2_Swagger

--- src/main/java/com/example/crudmysql/model/User.java
+++ src/main/java/com/example/crudmysql/model/User.java
@@ -15,6 +15,7 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
+    //@NotNull( = "First Name cannot be null")
     @Column(name = "first_name")
     private String firstName;
     @Column(name = "last_name")
